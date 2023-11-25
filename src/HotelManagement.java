0aa4808775d1fae582aa657c286a317de2495ade
Akash D <akashdgowda6361@gmail.com>
2023-11-25 14:40:10 +0530

    first change

---
+++ src/HotelManagement.java
@@ -0,0 +1,8 @@
+public class HotelManagement {
+    private static final String url = "jdbc:mysql://localhost:3306/hotel_db";
+    private static final String username = "root";
+    private static final String password = "MySQLPassword";
+    public static void main(String[] args) {
+
+    }
+}
